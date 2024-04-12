1187920862fd0d83c05f3e5b859e9d63cab05995
ostalean ostale <antonino.ostale@kellogg.northwestern.edu>
2024-04-12 16:09:04 +0000

    CRM Exercise

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,40 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS salespersons;
+
 -- CREATE TABLES
+CREATE TABLE salespersons (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  indsutry TEXT
+);
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  email TEXT,
+  address TEXT,
+  phone INTEGER,
+  company_id INTEGER,
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  type TEXT,
+  date TIME,
+  contact_id INTEGER,
+  salesperson_id INTEGER,
+  company_id INTEGER,
+  notes TEXT
+);
